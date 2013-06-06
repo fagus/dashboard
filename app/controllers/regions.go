562ae2d6a5552d5d523e92e2b1ab09b73e77064a
Brian Ketelsen <bketelsen@gmail.com>
2013-06-06 09:24:44 -0400

    regions stub out

--- app/controllers/regions.go
+++ app/controllers/regions.go
@@ -2,7 +2,7 @@ package controllers
 
 import (
 	"github.com/robfig/revel"
-
+	"github.com/skynetservices/dashboard/app/models"
 )
 
 type Regions struct {
@@ -10,6 +10,19 @@ type Regions struct {
 }
 
 func (c Regions) Index() revel.Result {
+	regions := models.GetRegions()
+	return c.Render(regions)
+}
+
+func (c Regions) Nodes(id string) revel.Result {
+
+	return c.Render()
+}
+func (c Regions) Instances(id string) revel.Result {
+
+	return c.Render()
+}
+func (c Regions) Services(id string) revel.Result {
 
 	return c.Render()
 }
